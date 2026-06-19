ec975d11494ca6c9eb9a305238482631de9fd0c6
Jacob-SSR <teepakorn.snis@gmail.com>
2026-06-19 13:26:10 +0700

    Remove secret-containing batch from tracking

--- sync-sheets.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-curl -s -X POST http://localhost/api/sync/service-unit-to-sheets ^
-  -H "x-sync-secret: ppchos10909" ^
-  -H "Content-Type: application/json" ^
-  >> C:\scripts\sheets-sync.log 2>&1
